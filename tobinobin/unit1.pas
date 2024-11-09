6a18690ea249d470f3ec32cd8949f5014c920787
Aleksandr Kiber <36310292+pricklly@users.noreply.github.com>
2024-11-09 06:23:37 +0300

    Add files via upload

---
+++ tobinobin/unit1.pas
@@ -0,0 +1,45 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    ed: TEdit;
+    Image1: TImage;
+    Image2: TImage;
+    mm: TMemo;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  mm.Lines.Add(ed.Text);
+end;
+
+end.
+
